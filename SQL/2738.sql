6ec06bfd93f130191bd1d1d9eb66c08f30e02f8e
Kusko99 <99877193+Kusko99@users.noreply.github.com>
2023-03-18 11:50:53 -0300

    Create 2738.sql

---
+++ SQL/2738.sql
@@ -0,0 +1,5 @@
+SELECT candidate.name, 
+ROUND((score.math*2 + score.specific*3 + score.project_plan*5) / 10.0, 2) AS avg
+FROM candidate,score
+WHERE candidate.id = score.candidate_id
+ORDER BY avg desc;
