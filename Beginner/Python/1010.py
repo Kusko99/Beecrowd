c84a506f4688238f94f76222ad5e255f90bb543b
Kusko99 <99877193+Kusko99@users.noreply.github.com>
2023-03-18 19:26:30 -0300

    Create 1010.py

---
+++ Beginner/Python/1010.py
@@ -0,0 +1,10 @@
+a1,b1,c1=input().split()
+a2,b2,c2=input().split()
+a1=int(a1)
+b1=int(b1)
+c1=float(c1)
+a2=int(a2)
+b2=int(b2)
+c2=float(c2)
+resultado=b1*c1+b2*c2
+print(f"VALOR A PAGAR: R$ {resultado:.2f}")
